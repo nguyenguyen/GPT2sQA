2b293a6627cf10316cf363fc537dd38890905ae4
unknown <nguyenit95@gmail.com>
2022-08-27 11:14:09 +0700

    Updated utils

--- gpt2sqa/squad/utils.py
+++ gpt2sqa/squad/utils.py
@@ -12,7 +12,7 @@ from gpt2sqa.tokenization import whitespace_tokenize, BasicTokenizer
 logger = logging.getLogger(__name__)
 
 
-def read_squad_examples(input_file, is_training, version_2_with_negative=True):
+def read_squad_examples(input_file, is_training, version_2_with_negative=False):
     """Read a SQuAD json file into a list of SquadExample."""
     with open(input_file, "r", encoding="utf-8") as reader:
         input_data = json.load(reader)["data"]
